982e0b732cbd3460c1e59b662b4706934d223abf
= <=>
2025-01-07 23:13:19 +0530

    learned customized react app

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,12 @@
 import Chai from './chai'
 
-
+const username = "daksh"
 function App() {
 
   return (
     <>
     <Chai />
-    <h1>this is for test </h1>
+    <h1>this is for test  {username}</h1>
     <p>hahahaha test para</p>
     </>
   )
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,8 +3,33 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// const reactElement = {
+//   type: 'a',
+//   props: {
+//       href: 'https://google.com',
+//       target: '_blank'
+//   },
+//   children: 'Click me to visit google'
+// }
+
+function MyApp(){
+  return(
+    <div>
+      <h1>Custom React App we made </h1>
+    </div>
+  )
+}
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+const anotherUser = "daksh"
+
+const reactElement = React.createElement(
+  'a',
+  {href:'https://youtube.com',target:'_blank'},
+  'click me to visit youtube',
+  anotherUser 
+)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  reactElement
 )
